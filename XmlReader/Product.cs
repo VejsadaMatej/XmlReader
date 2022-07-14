c1e9d4c39a9c84a3824c707aa6e14d01aae2fa0b
VejsadaVSE <vejm04@vse.cz>
2022-07-15 00:49:27 +0200

    Přidejte soubory projektu.

---
+++ XmlReader/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlReader
+{
+    internal class Product
+    {
+        public string Ean { get; set; }
+
+        public Product(string ean)
+        {
+            Ean = ean;
+        }
+
+        public override string ToString()
+        {
+            return Ean;
+        }
+    }
+}
---
+++ XmlReader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml;
+using XmlReader;
+
+const string dirPath = "c:\\Users\\matej\\Desktop\\XmlReader\\Top"; //constant with the location of our directory
+List<Product> products = new List<Product>();
+string ean;
+
+string[] allFilePaths = Directory.GetFiles(dirPath, "*.*", SearchOption.AllDirectories);
+foreach (string filePath in allFilePaths)
+{
+    string xmlFile = File.ReadAllText(filePath);
+    XmlDocument xmldoc = new XmlDocument();
+    xmldoc.LoadXml(xmlFile);
+    XmlNodeList nodeList = xmldoc.GetElementsByTagName("EAN");
+    foreach (XmlNode node in nodeList)
+    {
+        ean = node.InnerText;
+        products.Add(new Product(ean));
+    }
+}
+
+foreach (Product product in products)
+{
+    Console.WriteLine(product);
+}
