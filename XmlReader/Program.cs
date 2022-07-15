269dc0eeddf34a3a93f498b296be6c1357df1a80
vejm04@vse.cz <GorillaHannaH1974@>
2022-07-15 08:12:55 +0200

    separating methods

--- XmlReader/Program.cs
+++ XmlReader/Program.cs
@@ -3,16 +3,14 @@ using System.Xml;
 using XmlReader;
 
 const string dirPath = "..\\..\\..\\..\\Top"; //constant with the location of our directory
+const string name = "EAN"; //constant with the keyword we're looking for
 List<Product> products = new List<Product>();
 StringBuilder outcome = new StringBuilder();
 
 string[] allFilePaths = Directory.GetFiles(dirPath, "*.*", SearchOption.AllDirectories);
 foreach (string filePath in allFilePaths)
 {
-    string xmlFile = File.ReadAllText(filePath);
-    XmlDocument xmldoc = new XmlDocument();
-    xmldoc.LoadXml(xmlFile);
-    XmlNodeList nodeList = xmldoc.GetElementsByTagName("EAN");
+    XmlNodeList nodeList = GetNodeList(filePath, name);
     foreach (XmlNode node in nodeList)
     {
         products.Add(new Product(node.InnerText));
@@ -20,4 +18,13 @@ foreach (string filePath in allFilePaths)
     }
 }
 
-Console.WriteLine(outcome.ToString());
+XmlNodeList GetNodeList(string wantedFilePath, string wantedName)
+{
+    string xmlFile = File.ReadAllText(wantedFilePath);
+    XmlDocument xmldoc = new XmlDocument();
+    xmldoc.LoadXml(xmlFile);
+    XmlNodeList nodeList = xmldoc.GetElementsByTagName(wantedName);
+    return nodeList;
+}
+
+Console.Write(outcome.ToString());
